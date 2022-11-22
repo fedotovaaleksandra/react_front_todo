69766c7745c7b1c812de6b12d3194ee6f5accfa9
Alexandra Fedotova <fedotova.fortech@gmail.com>
2022-11-22 11:15:54 +0300

    feat created file style.scss

--- src/components/AddTask/AddTask.jsx
+++ src/components/AddTask/AddTask.jsx
@@ -6,6 +6,36 @@ const AddTask = ({ text, addTask, showError, deleteTask }) => {
   const [ addText, setAddText ] = useState(text);
 
   const  addNewTask = async () => {
-    
-  }
-} 
+     if (addText.trim() === "") {
+      showError("not be empty");
+      return;
+     }
+     const newTask = await addTask(addText);
+
+     if (newTask) {
+      setAddText("");
+      showError("");
+     }
+  };
+  
+  return (
+    <div className="control">
+      <h1 className="control_header"> To-Do List</h1>
+      <input 
+      className="control_input"
+      type="text"
+      value={addText}
+      onChange={(elem) => setAddText(elem.target.value)}
+      />
+      <button
+      type="button"
+      className="control_btn"
+      onClick={addNewTask}
+      >
+        Добавить задачу
+      </button>
+    </div>
+  );
+};
+
+export default AddTask;
